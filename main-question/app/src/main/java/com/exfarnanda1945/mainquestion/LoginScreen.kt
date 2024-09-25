633464078056df1f295a2cd7230aec3614ee14d6
miko <exfar@samir.co.id>
2024-09-25 13:42:18 +0700

    fix: save the username

--- main-question/app/src/main/java/com/exfarnanda1945/mainquestion/LoginScreen.kt
+++ main-question/app/src/main/java/com/exfarnanda1945/mainquestion/LoginScreen.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -28,15 +27,15 @@ import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 
-val Context.dataStore by preferencesDataStore(name = "isLogged")
-val key = booleanPreferencesKey("status")
+val Context.dataStore by preferencesDataStore(name = "user")
+val key = stringPreferencesKey("username")
 
 @Composable
 fun LoginScreen(modifier: Modifier) {
@@ -44,64 +43,88 @@ fun LoginScreen(modifier: Modifier) {
     val coroutineScope = rememberCoroutineScope()
     var username by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
-    val isLoggedIn by getFromDs(context).collectAsState(false)
+    val isLoggedIn by getFromDs(context).collectAsState("")
 
     Column(
-        modifier = Modifier
+        modifier = modifier
             .fillMaxSize()
             .padding(16.dp),
         verticalArrangement = Arrangement.Center
     ) {
-        OutlinedTextField(
-            value = username,
-            onValueChange = { username = it },
-            label = { Text("Username") },
-            modifier = Modifier.fillMaxWidth()
-        )
-        Spacer(modifier = Modifier.height(16.dp))
+        if (isLoggedIn.isEmpty()) {
+            OutlinedTextField(
+                value = username,
+                onValueChange = { username = it },
+                label = { Text("Username") },
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(16.dp))
 
-        OutlinedTextField(
-            value = password,
-            onValueChange = { password = it },
-            label = { Text("Password") },
-            visualTransformation = PasswordVisualTransformation(),
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-            modifier = Modifier.fillMaxWidth()
-        )
-        Spacer(modifier = Modifier.height(8.dp))
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Password") },
+                visualTransformation = PasswordVisualTransformation(),
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(10.dp))
 
-        Button(
-            onClick = {
-                if(username.isEmpty() || password.isEmpty()){
-                    Toast.makeText(context,"Email dan password tidak boleh kosong",Toast.LENGTH_SHORT).show()
-                }else{
-                    coroutineScope.launch {
-                        saveToDs(context, !isLoggedIn)
+            Button(
+                onClick = {
+                    if (username.isEmpty() || password.isEmpty()) {
+                        Toast.makeText(
+                            context,
+                            "Email dan password tidak boleh kosong",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    } else {
+                        coroutineScope.launch {
+                            saveToDs(context, username)
+                        }
                     }
-                }
-            },
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text(text = "Login")
+                },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(text = "Login")
+            }
+        } else {
+            Text("Selamat Datang, ${isLoggedIn}")
+            Spacer(modifier = Modifier.height(10.dp))
+            Button(
+                onClick = {
+                    if (username.isEmpty() || password.isEmpty()) {
+                        Toast.makeText(
+                            context,
+                            "Email dan password tidak boleh kosong",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    } else {
+                        coroutineScope.launch {
+                            saveToDs(context, "")
+                            username = ""
+                            password = ""
+                        }
+                    }
+                },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(text = "Logout")
+            }
         }
-        Spacer(modifier = Modifier.height(8.dp))
-        Text(
-            text = if (isLoggedIn) "Logged in as $username" else "Not logged in",
-            style = MaterialTheme.typography.bodyMedium
-        )
+
+
     }
 }
 
-suspend fun saveToDs(context: Context, value: Boolean) {
+suspend fun saveToDs(context: Context, value: String) {
     context.dataStore.edit { preferences ->
         preferences[key] = value
-
     }
 }
 
-fun getFromDs(context: Context): Flow<Boolean> {
+fun getFromDs(context: Context): Flow<String> {
     return context.dataStore.data.map { value: Preferences ->
-        value[key] ?: false
+        value[key] ?: ""
     }
 }
 
